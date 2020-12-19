94d49f9d2655c99fd6aa876aa5141d4b18765d0e
Josimar16 <josimarjr479@gmail.com>
2020-12-18 22:40:00 -0300

    :zap: inicializado o producer, iniciado a api

---
+++ api/src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { Kafka } from 'kafkajs';
+import routes from './routes';
+
+const app = express();
+
+/**
+ * Faz conexão com o kafka
+ */
+const kafka = new Kafka({
+  clientId: 'api',
+  brokers: ['kafka:9092']
+});
+
+const producer = kafka.producer();
+
+/**
+ * Disponibiliza o producer para todas as rotas 
+ */
+app.use((req, res, next) => {
+  req.producer = producer;
+
+  return next();
+});
+
+/**
+ * Cadastra as rotas da aplicação
+ */
+app.use(routes);
+
+async function run() {
+  // await producer.connect()
+
+  app.listen(3333)
+}
+
+run().catch(console.error)
+
