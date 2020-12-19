9c7e9e9d04e56ffd2f46e8c7f776762d6149c787
Josimar16 <josimarjr479@gmail.com>
2020-12-18 23:20:00 -0300

    estrutura para enviar a primeira mensagem com kafta

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,8 +3,19 @@ import express from 'express';
 const routes = express.Router();
 
 routes.post('/certification', async (req, res) => {
+  const message = {
+    user: { id: 1, name: 'Josimar Martins' },
+    course: 'Kafka com node.js',
+    grade: 5
+  }
   //Chamar micro serviço
-  return res.json({ ok: true })
+  await req.producer.send({
+    topic: 'issue-certificate',
+    messages: [
+      { value: JSON.stringify(message) }
+    ]
+  });
+  return res.json({ ok: true });
 })
 
 export default routes;
--- api/src/server.js
+++ api/src/server.js
@@ -9,7 +9,7 @@ const app = express();
  */
 const kafka = new Kafka({
   clientId: 'api',
-  brokers: ['kafka:9092']
+  brokers: ['localhost:9092']
 });
 
 const producer = kafka.producer();
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use(routes);
 
 async function run() {
-  // await producer.connect()
+  await producer.connect()
 
   app.listen(3333)
 }
